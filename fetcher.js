bf9190e3d85cf174ff7c8585a0359663e1bcc711
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-30 16:32:33 +0000

    let user know if URL is invalid

--- fetcher.js
+++ fetcher.js
@@ -7,13 +7,16 @@ const url = args[0];
 const localPath = args[1];
 
 request(url, (error, response, body) => {
-
+  if(response.statusCode !== 200) {
+    console.log(`URL is invalid. Status code: ${response.statusCode}`);
+    return
+  }
   if(fs.existsSync(localPath)) {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     });
-    
+
     rl.question(`File already exists at ${localPath}. Do you want to overwrite it? (Y/N) `, (answer) => {
       if(answer === 'Y') {
         fs.writeFile(localPath, body, err => {
