ae7e2af37cef04382a26b39f43e8ac1523ec974d
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-30 17:37:25 +0000

    Updated error code message and added comments

--- fetcher.js
+++ fetcher.js
@@ -2,15 +2,24 @@ const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
 
+// parse command line arguments
 let args = process.argv.slice(2);
 const url = args[0];
 const localPath = args[1];
 
 request(url, (error, response, body) => {
+  //URL errors
+  if(error) {
+    console.log(`URL is invalid. Error code: ${error.code}`);
+    return
+  }
   if(response.statusCode !== 200) {
     console.log(`URL is invalid. Status code: ${response.statusCode}`);
     return
   }
+  //File path error
+
+  //Ask user if they want to overwrite existing file
   if(fs.existsSync(localPath)) {
     const rl = readline.createInterface({
       input: process.stdin,
